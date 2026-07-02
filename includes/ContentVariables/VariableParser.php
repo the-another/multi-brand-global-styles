b528881b6877e13538cf84e66a5b0030294b62a9
Nemanja Cimbaljevic <zion.trooper@gmail.com>
2026-07-02 21:42:26 +0200

    feat: add VariableParser for key=value textarea input

---
+++ includes/ContentVariables/VariableParser.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Variable Parser Service
+ *
+ * @package MultiDomainGlobalStyles
+ * @since 1.0.0
+ */
+
+namespace TheAnother\Plugin\MultiDomainGlobalStyles\ContentVariables;
+
+/**
+ * Class VariableParser
+ *
+ * Parses "key = value" textarea input into a variable map.
+ */
+class VariableParser {
+
+	/**
+	 * Parse "key = value" lines into an associative array.
+	 *
+	 * @param string $raw Raw textarea contents.
+	 * @return array<string, string> Lowercased, alphanumeric+underscore key => trimmed value.
+	 */
+	public function parse( string $raw ): array {
+		$variables = array();
+
+		foreach ( preg_split( '/\r\n|\r|\n/', $raw ) as $line ) {
+			if ( ! str_contains( $line, '=' ) ) {
+				continue;
+			}
+
+			[ $key, $value ] = array_map( 'trim', explode( '=', $line, 2 ) );
+			$key             = strtolower( preg_replace( '/[^a-z0-9_]/i', '', $key ) );
+
+			if ( '' === $key ) {
+				continue;
+			}
+
+			$variables[ $key ] = $value;
+		}
+
+		return $variables;
+	}
+}
